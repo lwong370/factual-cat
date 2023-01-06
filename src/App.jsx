41135a15bed2a3e6dda40f00c7f09c29a03e0b35
Lana <lanawo370@gmail.com>
2023-01-05 19:21:22 -0800

    Added basic style: bkgrd, textbox, etc

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './App.css'
 
-const apiLey = "";
 const url = "https://catfact.ninja/fact?max_length=100";
 
 const App = () => {
+  const [fact, setFact] = useState("Domestic cats often recognize their own names.");
 
   const getFact = () => {
     console.log("Hello World");
+
 
     fetch(url)
       .then((res) => res.json())
       .then((cats) => {
         console.log('Cats: ', cats);
+        const fact = cats["fact"];
+        setFact(fact);
       })
       .catch((error) => {
+        setFact("Can't get any cat facts right now :(((")
         console.log('Error: ', error);
       });
   }
 
+  console.log("Cat fact: ", fact);
+
   return (
-    <div>        
-        <h1>Factual Cat</h1>
-        <button onClick={getFact}>Get the Faxs</button>
-    </div>
+    <html>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Sofia&display=swap" rel="stylesheet"/>
+      </head>
+      <header>
+          <h1 className="font-link">Factual Cat</h1>
+        <div class="textbox">        
+            <h3 class="fact">{fact}</h3>
+        </div>
+            <button className="btn" onClick={getFact}>Get the Faxs</button>
+      </header>
+    </html>
   )
 }
 
